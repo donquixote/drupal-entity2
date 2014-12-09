f20cc8331ec1af5b2f05e29e70756744221a2943
Andreas Hennings <andreas@dqxtech.net>
2014-12-09 23:09:28 +0100

    Add unit tests.

---
+++ tests/src/EntityWrapperTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\entity2\Tests;
+
+class EntityWrapperTest extends \PHPUnit_Framework_TestCase {
+
+  function testNodeWrapper() {
+    $node = (object)array(
+
+    );
+  }
+}
